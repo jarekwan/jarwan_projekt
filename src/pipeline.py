1c4b6f7bff183079271fd477dfce8695b5f61346
Jaroslaw Wanczewski <jarek.wanczewski@gmail.com>
2025-09-06 16:37:50 +0200

    Dodano RandomForestClassifier do pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -2,11 +2,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
-def make_pipeline(use_tree=False) -> Pipeline:
+def make_pipeline(model="logreg") -> Pipeline:
     """Tworzy pipeline: standaryzacja + klasyfikator"""
-    if use_tree:
+    if model == "tree":
         classifier = DecisionTreeClassifier(random_state=42)
+    elif model == "forest":
+        classifier = RandomForestClassifier(random_state=42)
     else:
         classifier = LogisticRegression(max_iter=100)
 
