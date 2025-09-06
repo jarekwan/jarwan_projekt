e4b4210e730179dbf3d793ea6422a1a48d9d1808
Jaroslaw Wanczewski <jarek.wanczewski@gmail.com>
2025-09-06 16:35:53 +0200

    Dodano nowy klasyfikator DecisionTree do pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,10 +1,16 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 
-def make_pipeline() -> Pipeline:
-    """Tworzy pipeline: standaryzacja + klasyfikacja"""
+def make_pipeline(use_tree=False) -> Pipeline:
+    """Tworzy pipeline: standaryzacja + klasyfikator"""
+    if use_tree:
+        classifier = DecisionTreeClassifier(random_state=42)
+    else:
+        classifier = LogisticRegression(max_iter=100)
+
     return Pipeline(steps=[
         ("scaler", StandardScaler()),
-        ("classifier", LogisticRegression(max_iter=100))
+        ("classifier", classifier)
     ])
